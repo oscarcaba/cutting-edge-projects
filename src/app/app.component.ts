8fba3ea1fcb3339f20f0f712ac5bf375f4dc132e
ocabrera <oscarcaba@hotmail.com>
2021-11-18 14:58:13 -0500

    carga de archivos desde json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from 'querystring';
 import { Observable } from 'rxjs';
-import * as Catalog from './config/catalog.json';
-import { Producto } from './model/product';
+import EmployeesJson from './config/catalog.json';
+//import { Producto } from './model/product';
+
+interface Producto {
+  id: string;
+  name: string;
+  price: number;
+  count: number;
+  type: string;
+  subType: string;
+  image: string
+}
 
 @Component({
   selector: 'app-root1',
@@ -9,24 +20,28 @@ import { Producto } from './model/product';
   styleUrls: ['./app.component.scss']
 })
 
+
+
 export class AppComponent  implements OnInit {
 
   title = 'Workspace Mario';
 
+  ListaProductos:Producto[] = EmployeesJson;   
+                                              
+
+  constructor() {
+    console.log(this.ListaProductos);
+
+  }
+
   ngOnInit(): void {
-    console.log('Componente AppComponent cargado')
+    console.log('Componente AppComponent cargado');
+    console.log(this.ListaProductos);
   }
  
   addToCart(){
     console.log("Aniadiendo producto a carro de compras");
   }
-  
-  
-  getProducts(){
-    return (Catalog as any).Item;
-  }
-
-
 }
 
 
